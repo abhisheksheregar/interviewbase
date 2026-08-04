0e2c60923a3932c0a91341b60585bca534acb608
AbhishekSheregar <abhi.sheregar@gmail.com>
2026-08-04 11:53:32 +0530

    api auth protected

--- API/Controllers/QuestionListController.cs
+++ API/Controllers/QuestionListController.cs
@@ -1,5 +1,6 @@
 using interviewbase.DTO;
 using interviewbase.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
